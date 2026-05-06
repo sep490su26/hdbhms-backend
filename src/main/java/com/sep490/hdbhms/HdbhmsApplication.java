a8a7c48046db1b22cdde7729b7a80094316c6d51
ITSEENDA <eendanomail@gmail.com>
2026-05-06 17:42:37 +0700

    chore: first commit

---
+++ src/main/java/com/sep490/hdbhms/HdbhmsApplication.java
@@ -0,0 +1,13 @@
+package com.sep490.hdbhms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdbhmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdbhmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sep490/hdbhms/HdbhmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep490.hdbhms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdbhmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
