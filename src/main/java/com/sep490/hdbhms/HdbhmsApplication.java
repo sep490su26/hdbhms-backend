7cef17243f9dc2f3c012045a466832951d1280d3
ITSEENDA <nguyentdbhe189016@fpt.edu.vn>
2026-05-16 18:39:29 +0700

    fix: errors in application

--- src/main/java/com/sep490/hdbhms/HdbhmsApplication.java
+++ src/main/java/com/sep490/hdbhms/HdbhmsApplication.java
@@ -1,9 +1,5 @@
 package com.sep490.hdbhms;
 
-import com.sep490.hdbhms.file.infrastructure.config.FileProperties;
-import com.sep490.hdbhms.identityandaccess.infrastructure.config.ResetPasswordConfig;
-import com.sep490.hdbhms.identityandaccess.infrastructure.config.security.AuthProperties;
-import com.sep490.hdbhms.shared.constant.Default;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
@@ -15,10 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.sep490.*"}, exclude = {RedisRepositoriesAutoConfiguration.class})
 @EnableConfigurationProperties({
-        AuthProperties.class,
-        Default.class,
-        FileProperties.class,
-        ResetPasswordConfig.class
+//        AuthProperties.class,
+//        Default.class,
+//        FileProperties.class,
+//        ResetPasswordConfig.class
 })
 public class HdbhmsApplication {
 
