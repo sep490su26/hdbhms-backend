dc4c9f8455fbdef887a71aa6b08e332262b281fa
nguyentdb <nguyentdbhe189016@fpt.edu.vn>
2026-05-23 17:57:21 +0700

    fix(occupancy): add missing modifications

--- src/main/java/com/sep490/hdbhms/occupancy/application/port/out/PropertyRepository.java
+++ src/main/java/com/sep490/hdbhms/occupancy/application/port/out/PropertyRepository.java
@@ -1,6 +1,9 @@
 package com.sep490.hdbhms.occupancy.application.port.out;
 
 import com.sep490.hdbhms.occupancy.domain.model.Property;
+import com.sep490.hdbhms.occupancy.domain.value_objects.PropertyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -8,4 +11,6 @@ public interface PropertyRepository {
     Property save(Property property);
 
     Optional<Property> findById(Long id);
+
+    Page<Property> findAll(PropertyStatus status, Pageable pageable);
 }
--- src/main/java/com/sep490/hdbhms/occupancy/infrastructure/persistence/repository/SpringDataPropertyRepository.java
+++ src/main/java/com/sep490/hdbhms/occupancy/infrastructure/persistence/repository/SpringDataPropertyRepository.java
@@ -2,13 +2,20 @@ package com.sep490.hdbhms.occupancy.infrastructure.persistence.repository;
 
 import com.sep490.hdbhms.occupancy.application.port.out.PropertyRepository;
 import com.sep490.hdbhms.occupancy.domain.model.Property;
+import com.sep490.hdbhms.occupancy.domain.value_objects.PropertyStatus;
+import com.sep490.hdbhms.occupancy.infrastructure.persistence.entity.PropertyEntity;
 import com.sep490.hdbhms.occupancy.infrastructure.persistence.jpa.JpaPropertyRepository;
 import com.sep490.hdbhms.occupancy.infrastructure.persistence.mapper.PropertyPersistenceMapper;
+import com.sep490.hdbhms.shared.specifications.PropertySpecifications;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -30,6 +37,18 @@ public class SpringDataPropertyRepository implements PropertyRepository {
     @Override
     public Optional<Property> findById(Long id) {
         return jpaPropertyRepository.findById(id)
+                .map(propertyPersistenceMapper::toDomain);
+    }
+
+    @Override
+    public Page<Property> findAll(PropertyStatus status, Pageable pageable) {
+        List<Long> ids = jpaPropertyRepository.findAll().stream()
+                .map(PropertyEntity::getId)
+                .toList();
+        Specification<PropertyEntity> specification = Specification
+                .where(PropertySpecifications.idIn(ids))
+                .and(PropertySpecifications.statusIn(status));
+        return jpaPropertyRepository.findAll(specification, pageable)
                 .map(propertyPersistenceMapper::toDomain);
     }
 }
