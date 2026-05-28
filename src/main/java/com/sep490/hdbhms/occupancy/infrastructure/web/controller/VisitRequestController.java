e07e2016d668cc2394b4a36f4b022b583736db7c
nguyentdb <nguyentdbhe189016@fpt.edu.vn>
2026-05-28 10:38:18 +0700

    chore: add new .env.template

--- src/main/java/com/sep490/hdbhms/occupancy/infrastructure/web/controller/VisitRequestController.java
+++ src/main/java/com/sep490/hdbhms/occupancy/infrastructure/web/controller/VisitRequestController.java
@@ -20,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
