bc6984618158aa82dd9a057e1d917c5a23c51b6d
nguyentdb <160340992+itsnguyentdb@users.noreply.github.com>
2026-06-28 06:35:37 +0700

    chore: remove unused files

--- src/main/java/com/sep490/hdbhms/modules/maintenance/dto/ConfirmAndReviewResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sep490.hdbhms.modules.maintenance.dto;
-
-public record ConfirmAndReviewResponse(
-        MaintenanceTicketActionResponse ticket,
-        MaintenanceTicketReviewResponse review
-) {
-}
--- src/main/java/com/sep490/hdbhms/modules/maintenance/dto/MaintenanceTicketActionResponse.java
+++
@@ -1,9 +0,0 @@
-package com.sep490.hdbhms.modules.maintenance.dto;
-
-
-public record MaintenanceTicketActionResponse(
-        Long id,
-        String status,
-        String statusLabel
-) {
-}
