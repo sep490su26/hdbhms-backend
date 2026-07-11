0b295777067551a16a27e7e9fcfe7344de8beda8
nguyentdb <160340992+itsnguyentdb@users.noreply.github.com>
2026-07-11 12:00:11 +0700

    fix: add missing files after reverting

---
+++ src/main/java/com/sep490/hdbhms/billingandpayment/domain/value_objects/CollectionAccountProvider.java
@@ -0,0 +1,9 @@
+package com.sep490.hdbhms.billingandpayment.domain.value_objects;
+
+public enum CollectionAccountProvider {
+    BANK,
+    MOMO,
+    ZALOPAY,
+    CASH,
+    OTHER
+}
---
+++ src/main/java/com/sep490/hdbhms/changerequest/domain/value_objects/AssignedRole.java
@@ -0,0 +1,7 @@
+package com.sep490.hdbhms.changerequest.domain.value_objects;
+
+public enum AssignedRole {
+    OWNER,
+    MANAGER,
+    ACCOUNTANT
+}
